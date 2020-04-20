5a6658d44e7e8832c8a5722e8bd45e9523ce4dba
kjoonk <kjoonk@gmail.com>
2020-04-20 17:09:49 +0900

    TableView, Navigation, DetailView 뼈대 구성

--- iOS/SideDish/SideDish/DetailViewController.swift
+++ iOS/SideDish/SideDish/DetailViewController.swift
@@ -2,13 +2,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
     }
 
-
 }
-
