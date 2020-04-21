6837a572573cc844dab6c0fb5b097d9607774b48
Byung Hyun Choi <choi8608@gmail.com>
2020-04-21 21:22:09 +0900

    [BE #24] Feature: Add skeleton code
    - 베스트 반찬 상세는 필요하지 않았습니다
    - DishMessages 추가

--- BE/src/main/java/com/codesquad/sidedish08/controller/BestDishController.java
+++ BE/src/main/java/com/codesquad/sidedish08/controller/BestDishController.java
@@ -1,5 +1,6 @@
 package com.codesquad.sidedish08.controller;
 
+import com.codesquad.sidedish08.msessage.DishMessages;
 import com.codesquad.sidedish08.msessage.SuccessMessages;
 import com.codesquad.sidedish08.response.ApiResponse;
 import com.codesquad.sidedish08.service.BasicService;
@@ -7,7 +8,6 @@ import com.codesquad.sidedish08.service.BestDishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,15 +22,9 @@ public class BestDishController {
     this.bestDishService = bestDishService;
   }
 
-  @ApiOperation(value = "", notes = "베스트 반찬")
+  @ApiOperation(value = "", notes = DishMessages.BEST_DISH)
   @GetMapping
   public ApiResponse dish() {
     return new ApiResponse(SuccessMessages.SUCCESS, bestDishService.dish());
-  }
-
-  @ApiOperation(value = "", notes = "베스트 반찬 상세")
-  @GetMapping("/{detailHash}")
-  public ApiResponse detail(@PathVariable String detailHash) {
-    return new ApiResponse(SuccessMessages.SUCCESS, bestDishService.detail(detailHash));
   }
 }
---
+++ BE/src/main/java/com/codesquad/sidedish08/msessage/DishMessages.java
@@ -0,0 +1,6 @@
+package com.codesquad.sidedish08.msessage;
+
+public class DishMessages {
+
+  public static final String BEST_DISH = "베스트 반찬 : parameter 로 들어오는 categoryId 를 모두 응답합니다";
+}
