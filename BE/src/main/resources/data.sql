40cf76708837e1e6abb88f6a24ad266e9b29bfd4
Byung Hyun Choi <choi8608@gmail.com>
2020-04-21 20:00:10 +0900

    [BE #17] Feature: Init DB
    - database 이름 sidedish_db 로 변경
    - category table, data 추가

---
+++ BE/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `category` (`id`, `type`)
+VALUES (1, '든든한 반찬');
+INSERT INTO `category` (`id`, `type`)
+VALUES (2, '국물 요리');
+INSERT INTO `category` (`id`, `type`)
+VALUES (3, '밑반찬');
---
+++ BE/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`
+(
+    `id`   BIGINT      NOT NULL,
+    `type` VARCHAR(45) NULL,
+    PRIMARY KEY (`id`)
+);
