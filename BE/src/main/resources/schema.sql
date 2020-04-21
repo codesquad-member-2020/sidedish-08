f584306b4bf3e09ba13a6e28807413aabc6ffa2b
Han <quaint3@gmail.com>
2020-04-21 22:08:45 +0900

    [BE #17] Chore: schema auto_increment 추가
    데이터 추가를 쉽게 하기 위해서 조건 추가

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `dish`;
 
 CREATE TABLE `category`
 (
-    `id`   BIGINT      NOT NULL,
+    `id`   BIGINT      NOT NULL AUTO_INCREMENT,
     `type` VARCHAR(45) NULL,
     PRIMARY KEY (`id`)
 );
@@ -11,11 +11,11 @@ CREATE TABLE `category`
 
 CREATE TABLE `dish`
 (
-    `id`           BIGINT       NOT NULL,
+    `id`           BIGINT       NOT NULL AUTO_INCREMENT,
     `detail_hash`  VARCHAR(45)  NULL,
     `title`        VARCHAR(100) NULL,
     `description`  VARCHAR(500) NULL,
-    `n_price`      INT          NULL,
+    `price`        INT          NULL,
     `category_id`  BIGINT       NOT NULL REFERENCES category (id),
     `category_key` int,
     PRIMARY KEY (`id`)
