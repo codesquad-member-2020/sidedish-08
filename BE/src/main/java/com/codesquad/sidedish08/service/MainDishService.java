81933d0d8c560a4f75dfe0621ded25666e39fd77
Han <quaint3@gmail.com>
2020-04-21 22:14:12 +0900

    [BE #17] Feature: DB에 있는 데이터 리턴하도록
    Category Repository를 통해서, 든든한 반찬에 해당하는 카테고리와 그에 딸린 dish들이 리턴되도록 설정

--- BE/src/main/java/com/codesquad/sidedish08/service/MainDishService.java
+++ BE/src/main/java/com/codesquad/sidedish08/service/MainDishService.java
@@ -2,15 +2,21 @@ package com.codesquad.sidedish08.service;
 
 import static com.codesquad.sidedish08.util.ResponseUtils.getResultMap;
 
-import com.codesquad.sidedish08.message.MockMessages;
+import com.codesquad.sidedish08.repository.CategoryRepository;
 import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainDishService implements BasicService {
 
+  private final CategoryRepository categoryRepository;
+
+  public MainDishService(CategoryRepository categoryRepository) {
+    this.categoryRepository = categoryRepository;
+  }
+
   public Map<String, ?> dish() {
-    return getResultMap("dish", MockMessages.mainDishString);
+    return getResultMap("dish", categoryRepository.findById(1L));
   }
 
   public Map<String, ?> detail(String detailHash) {
