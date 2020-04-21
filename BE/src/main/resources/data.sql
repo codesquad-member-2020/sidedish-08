c9dbcdd40522d5347c83b71a455a8a4db7794e74
Han <quaint3@gmail.com>
2020-04-21 22:09:36 +0900

    [BE #17] Chore: data 추가
    Category 수정, dish 데이터 추가

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,6 +1,19 @@
 INSERT INTO `category` (`id`, `type`)
-VALUES (1, '든든한 반찬');
+VALUES (null, '든든한 반찬');
 INSERT INTO `category` (`id`, `type`)
-VALUES (2, '국물 요리');
+VALUES (null, '국물 요리');
 INSERT INTO `category` (`id`, `type`)
-VALUES (3, '밑반찬');
+VALUES (null, '밑반찬');
+
+INSERT INTO `dish` (`id`, `detail_hash`, `title`, `description`, `price`, `category_id`, `category_key`)
+VALUES (null, 'HBDEF', '[미노리키친] 규동 250g', '일본인의 소울푸드! 한국인도 좋아하는 소고기덮밥',6500, 1, 0);
+
+INSERT INTO `dish` (`id`, `detail_hash`, `title`, `description`, `price`, `category_id`, `category_key`)
+VALUES (null, 'HDF73', '[빅마마의밥친구] 아삭 고소한 연근고기조림 250g', '편식하는 아이도 좋아하는 건강한 연근조림',5500, 1, 1);
+
+INSERT INTO `dish` (`id`, `detail_hash`, `title`, `description`, `price`, `category_id`, `category_key`)
+VALUES (null, 'HF778', '[소중한식사] 골뱅이무침 195g', '매콤새콤달콤, 반찬으로도 안주로도 좋은',7000, 1, 2);
+
+INSERT INTO `dish` (`id`, `detail_hash`, `title`, `description`, `price`, `category_id`, `category_key`)
+VALUES (null, 'HFB53', '[옹가솜씨] 꽁치간장조림 240g', '생강 향이 산뜻한',5800, 1, 3);
+
