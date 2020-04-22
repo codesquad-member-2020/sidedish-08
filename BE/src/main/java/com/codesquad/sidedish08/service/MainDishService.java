dd28c73168669c749f1d80fd61b244a8fe0b6624
Han <quaint3@gmail.com>
2020-04-22 22:32:05 +0900

    [BE #32] Refactor: api/main 수정
    리턴되는 객체 이름 변경, repository 변경

--- BE/src/main/java/com/codesquad/sidedish08/service/MainDishService.java
+++ BE/src/main/java/com/codesquad/sidedish08/service/MainDishService.java
@@ -3,6 +3,7 @@ package com.codesquad.sidedish08.service;
 import static com.codesquad.sidedish08.util.ResponseUtils.getResultMap;
 
 import com.codesquad.sidedish08.repository.CategoryRepository;
+import com.codesquad.sidedish08.repository.JdbcCategoryRepository;
 import java.util.Map;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +11,18 @@ import org.springframework.stereotype.Service;
 public class MainDishService implements BasicService {
 
   private final CategoryRepository categoryRepository;
+  private final JdbcCategoryRepository jdbcCategoryRepository;
 
-  public MainDishService(CategoryRepository categoryRepository) {
+  public MainDishService(CategoryRepository categoryRepository,
+      JdbcCategoryRepository jdbcCategoryRepository) {
     this.categoryRepository = categoryRepository;
+    this.jdbcCategoryRepository = jdbcCategoryRepository;
   }
 
+
   public Map<String, ?> dish() {
-    return getResultMap("dish", categoryRepository.findById(1L));
+//    return getResultMap("dish", categoryRepository.findById(1L));
+    return getResultMap("body", jdbcCategoryRepository.findById());
   }
 
   public Map<String, ?> detail(String detailHash) {
