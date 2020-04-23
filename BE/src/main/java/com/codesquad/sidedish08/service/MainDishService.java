aad4ff356c91af654d89786084613c791460dd4d
Han <quaint3@gmail.com>
2020-04-23 18:06:15 +0900

    [BE #32] Refactor: detail Service 수정
    return 되는 값을 수정하였음

--- BE/src/main/java/com/codesquad/sidedish08/service/MainDishService.java
+++ BE/src/main/java/com/codesquad/sidedish08/service/MainDishService.java
@@ -1,13 +1,10 @@
 package com.codesquad.sidedish08.service;
 
 
-import static com.codesquad.sidedish08.util.ResponseUtils.getResultMap;
-
 import com.codesquad.sidedish08.model.dto.Main;
 import com.codesquad.sidedish08.repository.CategoryRepository;
 import com.codesquad.sidedish08.repository.JdbcCategoryRepository;
 import java.util.List;
-import java.util.Map;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,7 +23,7 @@ public class MainDishService {
     return jdbcCategoryRepository.findById(1L);
   }
 
-  public Map<String, Object> detail(String detailHash) {
-    return getResultMap("dish", null);
+  public Main detail(String detailHash) {
+    return jdbcCategoryRepository.findByHash(detailHash);
   }
 }
