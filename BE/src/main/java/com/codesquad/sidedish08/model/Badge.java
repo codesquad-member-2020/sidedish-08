8ac1cc350363ead29dd671426b69df29c041df69
Han <quaint3@gmail.com>
2020-04-23 17:46:22 +0900

    [BE #32] Refactor: 생성자 인자 수정, toString 메서드 생성
    객체간 구별은 위하여 id도 생성시 받도록 수

--- BE/src/main/java/com/codesquad/sidedish08/model/Badge.java
+++ BE/src/main/java/com/codesquad/sidedish08/model/Badge.java
@@ -1,6 +1,8 @@
 package com.codesquad.sidedish08.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -13,12 +15,13 @@ public class Badge {
 
   private String type;
 
-  private Badge(String type) {
+  private Badge(Long id, String type) {
+    this.id = id;
     this.type = type;
   }
 
-  public static Badge of(String type) {
-    return new Badge(type);
+  public static Badge of(Long id, String type) {
+    return new Badge(id, type);
   }
 
   public Long getId() {
@@ -27,5 +30,13 @@ public class Badge {
 
   public String getType() {
     return type;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+        .append("id", id)
+        .append("type", type)
+        .toString();
   }
 }
--- BE/src/main/java/com/codesquad/sidedish08/model/Delivery.java
+++ BE/src/main/java/com/codesquad/sidedish08/model/Delivery.java
@@ -1,6 +1,8 @@
 package com.codesquad.sidedish08.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -13,12 +15,13 @@ public class Delivery {
 
   private String type;
 
-  private Delivery(String type) {
+  private Delivery(Long id, String type) {
+    this.id = id;
     this.type = type;
   }
 
-  public static Delivery of(String type) {
-    return new Delivery(type);
+  public static Delivery of(Long id, String type) {
+    return new Delivery(id, type);
   }
 
   public Long getId() {
@@ -27,5 +30,13 @@ public class Delivery {
 
   public String getType() {
     return type;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+        .append("id", id)
+        .append("type", type)
+        .toString();
   }
 }
