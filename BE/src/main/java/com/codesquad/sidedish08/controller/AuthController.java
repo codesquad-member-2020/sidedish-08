20d64af8f5fd7f928fdaf7a22fe99beb1481ac1d
Han <quaint3@gmail.com>
2020-04-26 22:04:30 +0900

    [BE #7] Feature : github oauth 기능 간단하게
    참고자료를 통해, github oauth기능을 구현함
    필요한 id, key, url등은 하드코딩으로 구성함
    작동하는지만 확인하기 위해서
    JWT토큰을 발행하면, header에 있는 토큰을 가져온다음에, payload중에 이메일이 login map에 등록되어있으면, 서비스 페이지로 넘겨주고, 토큰이 없거나, 혹은 토큰에 있는 이메일이 map에 없으면 getcode 과정을 통과하는 식으로 구성하면 되지 않을까 생각함

--- BE/src/main/java/com/codesquad/sidedish08/controller/AuthController.java
+++ BE/src/main/java/com/codesquad/sidedish08/controller/AuthController.java
@@ -1,19 +1,41 @@
 package com.codesquad.sidedish08.controller;
 
 import com.codesquad.sidedish08.message.AuthMessages;
-import com.codesquad.sidedish08.message.SuccessMessages;
-import com.codesquad.sidedish08.model.response.ApiResponse;
+import com.codesquad.sidedish08.model.Email;
 import com.codesquad.sidedish08.service.AuthService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.websocket.server.PathParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @Api(tags = "Prod")
-@RequestMapping("/api/auth")
+@RequestMapping("/api/oauth")
 public class AuthController {
+
+  private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+  private static Map<String, String> login = new HashMap<>();
 
   private final AuthService authService;
 
@@ -22,8 +44,75 @@ public class AuthController {
   }
 
   @ApiOperation(value = "", notes = AuthMessages.LOGIN)
-  @GetMapping("login")
-  public ApiResponse login() {
-    return ApiResponse.ok(SuccessMessages.SUCCESS, authService.login());
+  @GetMapping("/login")
+  public ResponseEntity login() {
+    return getCode();
+  }
+
+  @GetMapping("/github/callback")
+  public String callback(@PathParam("code") String code) {
+    String token = getAccessToken(code);
+    String email = enrollEmails(token);
+    return login.get(email);
+  }
+
+  private ResponseEntity<String> getCode() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+    UriComponents uri = UriComponentsBuilder
+        .fromHttpUrl("https://github.com/login/oauth/authorize")
+        .queryParam("client_id", "38d15de6e32724a43a4d")
+        .queryParam("scope", "user")
+        .build(false);
+
+    headers.setLocation(uri.toUri());
+
+    logger.debug("URL : {}", uri);
+    return new ResponseEntity<>(headers, HttpStatus.SEE_OTHER);
+  }
+
+  private String getAccessToken(String code) {
+    MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+    Map<String, String> header = new HashMap<>();
+    header.put("Accept", "application/json");
+    headers.setAll(header);
+
+    MultiValueMap<String, String> requestPayloads = new LinkedMultiValueMap<>();
+    Map<String, String> requestPayload = new HashMap<>();
+    requestPayload.put("client_id", "38d15de6e32724a43a4d");
+    requestPayload.put("client_secret", "219c33b31280356acbd56f8c2750ed0ff62398ab");
+    requestPayload.put("code", code);
+    requestPayloads.setAll(requestPayload);
+
+    HttpEntity<MultiValueMap> request = new HttpEntity<>(requestPayloads, headers);
+    ResponseEntity<HashMap> response = new RestTemplate()
+        .postForEntity("https://github.com/login/oauth/access_token", request, HashMap.class);
+
+    Map<String, String> map = (Map<String, String>) response.getBody();
+    String accessToken = map.get("access_token");
+    logger.debug("access Token : {}", accessToken);
+    return accessToken;
+  }
+
+  private String enrollEmails(String accessToken) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(
+        new MediaType("application", "json", StandardCharsets.UTF_8));
+    headers.add("Authorization", "bearer " + accessToken);
+
+    ResponseEntity<List> response = new RestTemplate().exchange(
+        "https://api.github.com/user/emails", HttpMethod.GET, new HttpEntity<String>(headers),
+        List.class);
+
+    ObjectMapper mapper = new ObjectMapper();
+    List<Email> emails = mapper.convertValue(
+        response.getBody(),
+        new TypeReference<List<Email>>() {
+        }
+    );
+    Email email = emails.get(0);
+    logger.debug("Email : {}", email.getEmail());
+    login.put(email.getEmail(), accessToken);
+    return email.getEmail();
   }
 }
