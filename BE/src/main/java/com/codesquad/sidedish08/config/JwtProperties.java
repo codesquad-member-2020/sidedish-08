6fb90713a9e4e255f0611a60c3ecef0aa43edabc
Byung Hyun Choi <choi8608@gmail.com>
2020-04-26 09:04:31 +0900

    [BE #7] Refactor: 민감 정보 yml 로 이동
    - 민감 정보 yml 로 이동
    - Add Properties files

---
+++ BE/src/main/java/com/codesquad/sidedish08/config/JwtProperties.java
@@ -0,0 +1,17 @@
+package com.codesquad.sidedish08.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+  @Value("${jwt.salt}")
+  private String salt;
+
+  public String getSalt() {
+    return salt;
+  }
+}
--- BE/src/main/java/com/codesquad/sidedish08/message/AuthMessages.java
+++ BE/src/main/java/com/codesquad/sidedish08/message/AuthMessages.java
@@ -9,9 +9,5 @@ public class AuthMessages {
   public static final String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
   public static final String USER_EMAIL_URL = "https://api.github.com/user/emails";
 
-  public static final String CLIENT_ID = "31ae5169a7dc37e19605";
-  public static final String CLIENT_SECRET = "43debfbf5b584d5a0c7b58d215c5462a29ecd5e1";
-
-  public static final String SALT = "sidedish08SecreatKey";
   public static final Long EXPIRED_TIME = 1000L * 60L * 30L;
 }
