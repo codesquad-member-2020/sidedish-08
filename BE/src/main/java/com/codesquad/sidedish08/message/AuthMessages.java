c3298b6735d78c7a1e0df53c01132ec55407e0a1
kiyoesjh <hyeyaa125@gmail.com>
2020-04-29 15:19:06 +0900

    [FE] BE, IOS 폴더삭제

--- BE/src/main/java/com/codesquad/sidedish08/message/AuthMessages.java
+++
@@ -1,6 +0,0 @@
-package com.codesquad.sidedish08.message;
-
-public class AuthMessages {
-
-  public static final String LOGIN = "Github 로그인";
-}
--- BE/src/main/java/com/codesquad/sidedish08/message/SuccessMessages.java
+++
@@ -1,6 +0,0 @@
-package com.codesquad.sidedish08.message;
-
-public class SuccessMessages {
-
-  public static final String SUCCESS = "SUCCESS";
-}
